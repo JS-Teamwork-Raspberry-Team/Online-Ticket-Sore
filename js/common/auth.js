fd351cc82bbba54a2b7eec66537b7fcbba2606ae
Todor Vasilev <bggadbg@abv.bg>
2017-08-21 11:28:54 +0300

    updated home and login pages

---
+++ js/common/auth.js
@@ -0,0 +1,47 @@
+let auth = (() => {
+    // user/login
+    function login(username, password) {
+        let userData = {
+            username,
+            password
+        };
+
+        return requester.post('user', 'login', 'basic', userData);
+    }
+
+    // user/register
+    function register(username, password) {
+        let userData = {
+            username,
+            password
+        };
+
+        return requester.post('user', '', 'basic', userData);
+    }
+
+    // user/logout
+    function logout() {
+        let logoutData = {
+            authtoken: sessionStorage.getItem('authtoken')
+        };
+
+        return requester.post('user', '_logout', 'kinvey', logoutData);
+    }
+
+    function saveSession(userInfo) {
+        let id = userInfo['_id'];
+        let username = userInfo['username'];
+        let authtoken = userInfo['_kmd']['authtoken'];
+
+        sessionStorage.setItem('id', id);
+        sessionStorage.setItem('username', username);
+        sessionStorage.setItem('authtoken', authtoken);
+    }
+
+    return {
+        login,
+        register,
+        logout,
+        saveSession
+    }
+})();
